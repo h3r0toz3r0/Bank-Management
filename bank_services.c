47dca53ca68e65e0550b2eed66fc330b28b3cc33
h3r0toz3r0 <annadevries4@outlook.com>
2021-08-03 16:51:37 -0700

    skelton added for program, part 1. Function descriptions provided. All funtions set to return at the moment.

---
+++ bank_services.c
@@ -0,0 +1,69 @@
+//////////////////////////////////////////////////////////////
+//              Bank Management Program                     //
+//              Author: Anna DeVries                        //
+//              Date: 3 August 2021                         //
+//////////////////////////////////////////////////////////////
+
+/*  new_acc() function:
+    This function creates a new customer account. It asks 
+    for some  personal and banking details of the customer 
+    such as name, date of birth, citizenship number, 
+    address and phone number. You can enter the amount to 
+    deposit and choose one type of deposit account – saving, 
+    current, fixed for 1 year, fixed for 2 years or fixed 
+    for 3 years.
+*/
+void new_acc(void){
+    return;
+}
+
+/*  view_list() function:
+    With this function, you can view the customer’s banking 
+    information such as account number, name, address and 
+    phone number provided while creating the account.
+*/
+void view_list(void){
+    return;
+}
+
+/*  edit() function:
+    This function has been used for changing the address 
+    and phone number of a particular customer account.
+*/
+void edit(void){
+    return;
+}
+
+/*  transact() function:
+    With this function, you can deposit and withdraw 
+    money to and from a particular customer account.
+*/
+void transact(void){
+    return;
+}
+
+/*  erase() function:
+    This function is for deleting a customer account.
+*/
+void erase(void){
+    return;
+}
+
+/*  see() function:
+    This function shows account number, name, date of 
+    birth, citizenship number, age, address, phone 
+    number, type of account, amount deposited and date 
+    of deposit. It also displays the amount of interest 
+    corresponding to a particular account type.
+*/
+void see(void){
+    return;
+}
+
+/*  main() function:
+    This function displays the menu or welcome screen to 
+    perform different banking activities mentioned below.
+*/
+void new_acc(void){
+    return 0;
+}
